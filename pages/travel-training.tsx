a864c412c4000d8d6cb0926168e165d04854a699
Dustin Kiselbach <dustinkiselbach@gmail.com>
2021-05-06 20:48:08 -0400

    chore: remove unused import and semi colons

--- pages/travel-training.tsx
+++ pages/travel-training.tsx
@@ -1,67 +1,67 @@
-import { Entry } from 'contentful'
-import { lighten, rgba } from 'polished'
-import styled from 'styled-components'
-import { transportationCMS } from '../cms/transportationCMS'
-import { Layout } from '../components/Layout'
-import { SectionHeader } from '../components/SectionHeader'
-import { ContentfulTravelTraining } from '../types/Contentful'
+import { Entry } from "contentful";
+import { lighten } from "polished";
+import styled from "styled-components";
+import { transportationCMS } from "../cms/transportationCMS";
+import { Layout } from "../components/Layout";
+import { SectionHeader } from "../components/SectionHeader";
+import { ContentfulTravelTraining } from "../types/Contentful";
 
-import { Button } from '../components/Button'
-import { useRouter } from 'next/router'
+import { Button } from "../components/Button";
+import { useRouter } from "next/router";
 
 interface TravelTrainingProps {
-  travelTraining: Entry<ContentfulTravelTraining>
+  travelTraining: Entry<ContentfulTravelTraining>;
 }
 
 const TravelTraining = ({ travelTraining }: TravelTrainingProps) => {
-  const router = useRouter()
+  const router = useRouter();
   const {
-    fields: { description, flyer }
-  } = travelTraining
+    fields: { description, flyer },
+  } = travelTraining;
   return (
     <Layout>
-      <SectionHeader title='Travel Training'>
+      <SectionHeader title="Travel Training">
         <TravelTrainingDescription>
           <p>{description}</p>
           <a href={flyer.fields.file.url}>{flyer.fields.title}</a>
         </TravelTrainingDescription>
-        <Button text='Contact Us' onSubmit={() => router.push('/contact')} />
+        <Button text="Contact Us" onSubmit={() => router.push("/contact")} />
       </SectionHeader>
     </Layout>
-  )
-}
+  );
+};
 
-export async function getStaticProps () {
+export async function getStaticProps() {
   const res = await transportationCMS.getEntries<ContentfulTravelTraining>({
-    content_type: 'travelTraining'
-  })
+    content_type: "travelTraining",
+  });
 
-  const travelTraining = res.items[0]
+  const travelTraining = res.items[0];
 
   return {
     revalidate: 60 * 10,
     props: {
-      travelTraining
-    }
-  }
+      travelTraining,
+    },
+  };
 }
 
 const TravelTrainingDescription = styled.div`
   font-size: 1.5rem;
   margin-bottom: 2rem;
-  
+
   p {
     margin-bottom: 2rem;
   }
   a {
-    color: ${props => props.theme.colors.colorText};
+    color: ${(props) => props.theme.colors.colorText};
     text-decoration: underline;
 
     &:hover {
-      color: ${props => lighten(0.25, props.theme.colors.colorText)};
+      color: ${(props) => lighten(0.25, props.theme.colors.colorText)};
     }
     transition: all 0.2s ease-in-out;
   }
-`
+`;
 
-export default TravelTraining
+export default TravelTraining;
